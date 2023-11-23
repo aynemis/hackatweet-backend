378558a8f74e2b77e13694a928b0b4f34e3da5d9
Your name <Your email>
2023-11-23 16:59:07 +0100

    backend

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
-require('../hackatweet-backend/models/connection')
+require('../backend/models/connection')
 
 
 var indexRouter = require('./routes/index');
