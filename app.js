d33fcc208f0e69b4a11356dcc6bcc81120545698
Houss82 <houssem1982mansour@gmail.com>
2023-11-24 11:50:12 +0100

    11h50

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 var express = require('express');
-var cors = require('cors');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
 require('../hackatweet-backend/models/connection')
 
-var app = express();
-const cors = require('cors');
-app.use(cors());
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var app = express();
+const cors = require('cors');
+app.use(cors());
 
 
 app.use(logger('dev'));
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
